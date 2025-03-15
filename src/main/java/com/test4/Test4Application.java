3f2723c3decb72c357649fd2050884d7fe36aaf9
SONU KUMAR <kchandramani85@gmail.com>
2025-03-15 12:55:42 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -9,6 +9,7 @@ public class Test4Application {
 	public static void main(String[] args) {
 		int x = 100; 
 		int y = 200;
+		char xx = 's';
 		SpringApplication.run(Test4Application.class, args);
 	}
 
